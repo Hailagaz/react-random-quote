6a2bdd7c091b575df2b7a0ec5206907f4d51eefa
Oleh Cherniavskyi <oleg.cherniavsky.work@gmail.com>
2023-09-15 14:03:52 +0300

    add partitions background

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -5,7 +5,8 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import BackgroundSlider from 'react-background-slider';
+import Particles from 'react-tsparticles';
+import { loadSlim } from 'tsparticles-slim';
 
 function QuoteCard({ quote, fetchRandomQuote }) {
 	return (
@@ -77,30 +78,106 @@ function App() {
 		fetchRandomQuote();
 	}, []);
 
-	const images = [
-		'https://images-assets.nasa.gov/image/PIA12348/PIA12348~medium.jpg',
-		'https://images-assets.nasa.gov/image/PIA15416/PIA15416~medium.jpg',
-		'https://images-assets.nasa.gov/image/0302063/0302063~medium.jpg',
-		'https://images-assets.nasa.gov/image/PIA03606/PIA03606~medium.jpg',
-		'https://images-assets.nasa.gov/image/PIA03096/PIA03096~medium.jpg',
-	];
+	const particlesInit = async (engine) => {
+		await loadSlim(engine);
+	};
+
+	const particlesLoaded = async (container) => {
+		console.log(container);
+	};
 
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				minHeight: '100vh',
-				paddingTop: '200px',
-			}}
-		>
-			{/* Add BackgroundSlider component for animated background */}
-			<BackgroundSlider images={images} duration={5} transition={2} />
-			<Typography variant="h4" gutterBottom>
-				Random Quote Machine
-			</Typography>
-			<QuoteCard quote={quote} fetchRandomQuote={fetchRandomQuote} />
+		<div>
+			{/* Add the Particles component here */}
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={{
+					background: {
+						color: {
+							value: '#0d47a1',
+						},
+					},
+					fpsLimit: 120,
+					interactivity: {
+						events: {
+							onClick: {
+								enable: true,
+								mode: 'push',
+							},
+							onHover: {
+								enable: true,
+								mode: 'repulse',
+							},
+							resize: true,
+						},
+						modes: {
+							push: {
+								quantity: 4,
+							},
+							repulse: {
+								distance: 200,
+								duration: 0.4,
+							},
+						},
+					},
+					particles: {
+						color: {
+							value: '#ffffff',
+						},
+						links: {
+							color: '#ffffff',
+							distance: 150,
+							enable: true,
+							opacity: 0.5,
+							width: 1,
+						},
+						move: {
+							direction: 'none',
+							enable: true,
+							outModes: {
+								default: 'bounce',
+							},
+							random: false,
+							speed: 6,
+							straight: false,
+						},
+						number: {
+							density: {
+								enable: true,
+								area: 800,
+							},
+							value: 80,
+						},
+						opacity: {
+							value: 0.5,
+						},
+						shape: {
+							type: 'circle',
+						},
+						size: {
+							value: { min: 1, max: 5 },
+						},
+					},
+					detectRetina: true,
+				}}
+			/>
+
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					minHeight: '100vh',
+					paddingTop: '200px',
+				}}
+			>
+				<Typography variant="h4" gutterBottom>
+					Random Quote Machine
+				</Typography>
+				<QuoteCard quote={quote} fetchRandomQuote={fetchRandomQuote} />
+			</div>
 		</div>
 	);
 }
