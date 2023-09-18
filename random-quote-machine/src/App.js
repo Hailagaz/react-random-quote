4bc03e4bbd6e6b8f89b76dde8a59e36b3cd0be14
Oleh Cherniavskyi <oleg.cherniavsky.work@gmail.com>
2023-09-18 13:00:13 +0300

    fixes

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setQuote } from './redux/quoteSlice';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import ParticlesBackground from './components/Particles';
 import QuoteCard from './components/QuoteCard';
-import quotesData from './data/quotes.json';
+import axios from 'axios';
 
 function App() {
-	const quote = useSelector((state) => state.quote);
-	const dispatch = useDispatch();
+	const [quote, setQuote] = useState({ text: '', author: '' });
 
-	const fetchRandomQuote = () => {
-		const randomIndex = Math.floor(Math.random() * quotesData.length);
-		const randomQuote = quotesData[randomIndex];
+	const fetchRandomQuote = async () => {
+		try {
+			const response = await axios.get('https://api.quotable.io/random');
+			const { content, author } = response.data;
 
-		dispatch(setQuote({ text: randomQuote.text, author: randomQuote.author }));
+			setQuote({ text: content, author });
+		} catch (error) {
+			console.error('Error fetching quote:', error);
+		}
 	};
 
 	useEffect(() => {
 		fetchRandomQuote();
 	}, []);
+
+	const tweetQuote = () => {
+		const tweetText = `"${quote.text}" - ${quote.author}`;
+		const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+		window.open(tweetUrl, '_blank');
+	};
 
 	return (
 		<div>
@@ -30,22 +38,56 @@ function App() {
 					display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'center',
-					minHeight: '100vh',
-					paddingTop: '200px',
+					justifyContent: 'center',
+					minHeight: '97vh',
 				}}
 			>
-				<Typography variant="h4" gutterBottom>
-					Random Quote Machine
-				</Typography>
-				<Button
-					id="new-quote"
-					variant="contained"
-					color="primary"
-					onClick={fetchRandomQuote}
+				<Container
+					maxWidth="sm"
+					sx={{
+						padding: '20px',
+						textAlign: 'center',
+					}}>
+					<Button
+						id="new-quote"
+						variant="contained"
+						color="primary"
+						onClick={fetchRandomQuote}
+						maxWidth="50px"
+					>
+						New Quote
+					</Button>
+					<Button
+						id="tweet-quote"
+						variant="contained"
+						color="secondary"
+						onClick={tweetQuote}
+						maxWidth="50px"
+					>
+						Tweet Quote
+					</Button>
+				</Container>
+				<Container
+					maxWidth="sm"
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+						backgroundColor: 'rgba(255, 255, 255, 0.5)',
+						backdropFilter: 'blur(10px)',
+						borderRadius: '10px',
+						boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+						padding: '20px',
+						textAlign: 'center',
+						height: '200px',
+						overflowY: 'auto',
+					}}
 				>
-					New Quote
-				</Button>
-				<QuoteCard quote={quote} />
+					<QuoteCard quote={quote} />
+				</Container>
+
+
 			</div>
 		</div>
 	);
